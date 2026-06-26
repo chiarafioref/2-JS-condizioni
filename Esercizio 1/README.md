19938c973a698c8075e1211463a4c731e3690f46
Chiara <chiarafioref@gmail.com>
2026-06-26 17:39:34 +0200

    first commit

---
+++ Esercizio 1/README.md
@@ -0,0 +1,3 @@
+# ESERCIZIO
+
+Chiedi l'età dell'utente tramite prompt. Se l'utente è maggiorenne, stampa a console "Accesso consentito al locale".
---
+++ Esercizio 1/main.js
@@ -0,0 +1,9 @@
+console.log("script JS caricato");
+
+let age = Number(prompt("Quanti anni hai?"));
+
+if (age >= 18) {
+    console.log("Accesso consentito al locale.");
+} else {
+    console.log("Accesso negato ai minorenni.");
+}
